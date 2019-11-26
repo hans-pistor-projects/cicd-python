7b035f655b7994af4b2206aaff8fefe4b120ad2b
Hans Pistor <hpistor@ad.stetson.edu>
2019-11-26 14:20:06 -0500

    Make it pretty

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
 import os
-from flask import Flask
+import signal
+from flask import Flask, render_template
 from buzz import generator
 
 app = Flask(__name__)
 
+signal.signal(signal.SIGINT, lambda s, f: os.exit(0))
+
 
 @app.route("/")
 def generate_buzz():
-    page = '<html><body><h1>'
-    page += generator.generate_buzz()
-    page += "</h1></body></html>"
-    return page
+    buzz = generator.generate_buzz()
+    return render_template('index.html', buzz=buzz)
 
 
 if __name__ == "__main__":
